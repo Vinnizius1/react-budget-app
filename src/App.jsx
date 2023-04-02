d223fb193b5359dd32ad5df8c8eae92fc8b1586c
vinicius <vmm_7@hotmail.com>
2023-04-02 20:54:25 -0300

    Lição 2: errorElement: <Error />

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Routes
 import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Dashboard />,
     loader: dashboardLoader,
+    errorElement: <Error />,
   },
 ]);
 
