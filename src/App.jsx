6451eb5d5541a6f18efb2d39567460238efcadd9
vinicius <vmm_7@hotmail.com>
2023-04-02 23:16:52 -0300

    Lição 3: logout.js, Nav.jsx, heroicons...

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Layouts
+import Main, { mainLoader } from "./layouts/Main";
+
+// Actions
+import { logoutAction } from "./actions/logout";
 
 // Routes
 import Dashboard, { dashboardLoader } from "./pages/Dashboard";
@@ -7,9 +12,21 @@ import Error from "./pages/Error";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
-    loader: dashboardLoader,
+    element: <Main />,
+    loader: mainLoader,
     errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
+      },
+    ],
   },
 ]);
 
