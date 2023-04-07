e37572793666d82eed51d7d17ef4934caf66a1b6
vinicius <vmm_7@hotmail.com>
2023-04-07 18:41:17 -0300

    #6 - Custom Error Page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { useRouteError, Link, useNavigate } from "react-router-dom";
+
+// library imports
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 
 function Error() {
-  return <div>Error</div>;
+  const error = useRouteError();
+  const navigate = useNavigate();
+
+  return (
+    <div className="error">
+      <h1>Temos um problema!</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md">
+        <button className="btn btn--dark" onClick={() => navigate(-1)}>
+          <ArrowUturnLeftIcon width={20} />
+          <span>Go back</span>
+        </button>
+        <Link to="/" className="btn btn--dark">
+          <HomeIcon width={20} />
+          <span>Go home</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Error;
